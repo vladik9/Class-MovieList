aa5ae482dec0b27354cabb7c431b3c4b8f5a1530
vladik9 <v.cornici@gmail.com>
2021-05-22 16:50:59 +0300

    delet trash

--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
- colection_list.addToListElement(element_one);
-     colection_list.addToListElement(element_two);
-     colection_list.addToListElement(element_three);
-
-     colection_list.printElements(3);
